94f5cce6cd89a5893bda980e61e56ea4b8aed38e
Jan Christoph Bernack <jc.bernack@gmail.com>
2017-11-27 17:17:55 +0100

    Refactor to new code style

--- coap/coap-in.js
+++ coap/coap-in.js
@@ -1,101 +1,102 @@
-module.exports = function(RED) {
-    "use strict";
-    var coap = require('coap');
+const coap = require('coap');
 
-    // A node red node that sets up a local coap server
-    function CoapServerNode(n) {
-        // Create a RED node
-        RED.nodes.createNode(this,n);
-        var node = this;
+module.exports = function (RED) {
+  // A node red node that sets up a local coap server
+  function CoapServerNode(n) {
+    // Create a RED node
+    RED.nodes.createNode(this, n);
+    const node = this;
 
-        // Store local copies of the node configuration (as defined in the .html)
-        node.options = {};
-        node.options.name = n.name;
-        node.options.port = n.port;
+    // Store local copies of the node configuration (as defined in the .html)
+    node.options = {};
+    node.options.name = n.name;
+    node.options.port = n.port;
 
-        node._inputNodes = [];    // collection of "coap in" nodes that represent coap resources
+    // collection of 'coap in' nodes that represent coap resources
+    node.inputNodes = [];
 
-        // Setup node-coap server and start
-        node.server = new coap.createServer();
-        node.server.on('request', function(req, res) {
-            node.handleRequest(req, res);
-            res.on('error', function(err) {
-                node.log('server error');
-                node.log(err);
-            });
-        });
-        node.server.listen(node.options.port, function() {
-            //console.log('server started');
-            node.log('CoAP Server Started');
-        });
+    // Setup node-coap server and start
+    node.server = coap.createServer();
+    node.server.on('request', (req, res) => {
+      node.handleRequest(req, res);
+      res.on('error', err => {
+        node.log('server error');
+        node.log(err);
+      });
+    });
+    node.server.listen(node.options.port, () => {
+      // console.log('server started');
+      node.log('CoAP Server Started');
+    });
 
-        node.on("close", function() {
-            node._inputNodes = [];
-            node.server.close();
-        });
+    node.on('close', () => {
+      node.inputNodes = [];
+      node.server.close();
+    });
+  }
+
+  RED.nodes.registerType('coap-server', CoapServerNode);
+
+  CoapServerNode.prototype.registerInputNode = function (/*Node*/resource) {
+    let exists = false;
+    for (let i = 0; i < this.inputNodes.length; i++) {
+      if (this.inputNodes[i].options.url === resource.options.url &&
+        this.inputNodes[i].options.method === resource.options.method) {
+        exists = true;
+
+        // TODO: Does this have any effect? Should show the error in the frontend somehow? Some kind of status bar?
+        this.error('Node with the specified URL and Method already exists!');
+      }
     }
-    RED.nodes.registerType("coap-server",CoapServerNode);
-
-    CoapServerNode.prototype.registerInputNode = function(/*Node*/resource){
-        var exists = false;
-        for (var i = 0; i < this._inputNodes.length; i++) {
-            if (this._inputNodes[i].options.url == resource.options.url &&
-                this._inputNodes[i].options.method == resource.options.method) {
-                exists = true;
-
-                //TODO: Does this have any effect? Should show the error in the frontend somehow? Some kind of status bar?
-                this.error("Node with the specified URL and Method already exists!");
-            }
-        }
-        if (!exists) {
-            this._inputNodes.push(resource);
-        }
-    };
-
-    CoapServerNode.prototype.handleRequest = function(req, res){
-
-        //TODO: Check if there are any matching resource. If the resource is .well-known return the resource directory to the client
-        var matchResource = false;
-        var matchMethod = false;
-        for (var i = 0; i < this._inputNodes.length; i++) {
-            if (this._inputNodes[i].options.url == req.url) {
-                matchResource = true;
-                if (this._inputNodes[i].options.method == req.method) {
-                    matchMethod = true;
-                    var inNode = this._inputNodes[i];
-                    inNode.send({'req': req, 'res': res});
-                }
-            }
-        }
-        if (!matchResource) {
-            res.code = '4.04';
-            return res.end();
-        }
-
-        if (!matchMethod) {
-            res.code = '4.05';
-            return res.end();
-        }
-    };
-
-    function CoapInNode(n) {
-        RED.nodes.createNode(this,n);
-
-        //copy "coap in" node configuration locally
-        this.options = {};
-        this.options.method = n.method;
-        this.options.name = n.name;
-        this.options.server = n.server;
-        this.options.url = n.url.charAt(0) == "/" ? n.url : "/" + n.url;
-
-        this.serverConfig = RED.nodes.getNode(this.options.server);
-
-        if (this.serverConfig) {
-            this.serverConfig.registerInputNode(this);
-        } else {
-            this.error("Missing server configuration");
-        }
-
+    if (!exists) {
+      this.inputNodes.push(resource);
     }
-    RED.nodes.registerType("coap in",CoapInNode);
+  };
+
+  CoapServerNode.prototype.handleRequest = function (req, res) {
+    // TODO: If the resource is .well-known return the resource directory to the client
+    // Check if there are any matching resource.
+    let matchResource = false;
+    let matchMethod = false;
+    for (let i = 0; i < this.inputNodes.length; i++) {
+      if (this.inputNodes[i].options.url === req.url) {
+        matchResource = true;
+        if (this.inputNodes[i].options.method === req.method) {
+          matchMethod = true;
+          const inNode = this.inputNodes[i];
+          inNode.send({ req, res });
+        }
+      }
+    }
+    if (!matchResource) {
+      res.code = '4.04';
+      res.end();
+    }
+
+    if (!matchMethod) {
+      res.code = '4.05';
+      res.end();
+    }
+  };
+
+  function CoapInNode(n) {
+    RED.nodes.createNode(this, n);
+
+    // copy 'coap in' node configuration locally
+    this.options = {};
+    this.options.method = n.method;
+    this.options.name = n.name;
+    this.options.server = n.server;
+    this.options.url = n.url.charAt(0) === '/' ? n.url : '/' + n.url;
+
+    this.serverConfig = RED.nodes.getNode(this.options.server);
+
+    if (this.serverConfig) {
+      this.serverConfig.registerInputNode(this);
+    } else {
+      this.error('Missing server configuration');
+    }
+  }
+
+  RED.nodes.registerType('coap in', CoapInNode);
 };
--- coap/coap-request.js
+++ coap/coap-request.js
@@ -1,136 +1,138 @@
-module.exports = function(RED) {
-    "use strict";
+const coap = require('coap');
+const cbor = require('cbor');
+const url = require('uri-js');
+const linkFormat = require('h5.linkformat');
 
-    var coap = require('coap');
-    var cbor = require('cbor');
-    var url = require('uri-js');
-    var linkFormat = require('h5.linkformat');
+module.exports = function (RED) {
+  coap.registerFormat('application/cbor', 60);
 
-    coap.registerFormat('application/cbor', 60);
+  function CoapRequestNode(n) {
+    RED.nodes.createNode(this, n);
+    const node = this;
 
-    function CoapRequestNode(n) {
-        RED.nodes.createNode(this, n);
-        var node = this;
+    // copy 'coap request' configuration locally
+    node.options = {};
+    node.options.method = n.method;
+    node.options.observe = n.observe;
+    node.options.observe_on_start = n.observe_on_start || false;
+    node.options.name = n.name;
+    node.options.url = n.url;
+    node.options.contentFormat = n['content-format'];
+    node.options.rawBuffer = n['raw-buffer'];
 
-        // copy "coap request" configuration locally
-        node.options = {};
-        node.options.method = n.method;
-        node.options.observe = n.observe;
-	    node.options.observe_on_start = n.observe_on_start||false;
-        node.options.name = n.name;
-        node.options.url = n.url;
-        node.options.contentFormat = n['content-format'];
-        node.options.rawBuffer = n['raw-buffer'];
-
-        function _constructPayload(msg, contentFormat) {
-            var payload = null;
-
-            if (contentFormat === 'text/plain') {
-                payload = msg.payload;
-            } else if (contentFormat === 'application/json') {
-                payload = JSON.stringify(msg.payload);
-            } else if (contentFormat === 'application/cbor') {
-                payload = cbor.encode(msg.payload);
-            }
-
-            return payload;
-        }
-
-        function _makeRequest(msg) {
-            var reqOpts = url.parse(node.options.url || msg.url);
-            reqOpts.pathname = reqOpts.path;
-            reqOpts.method = ( node.options.method || msg.method || 'GET' ).toUpperCase();
-            reqOpts.headers = {};
-            reqOpts.headers['Content-Format'] = node.options.contentFormat;
-
-            function _onResponse(res) {
-
-                function _send(payload) {
-                    node.send(Object.assign({}, msg, {
-                        payload: payload,
-                        headers: res.headers,
-                        statusCode: res.code,
-                    }));
-                }
-
-                function _onResponseData(data) {
-					node.status({fill:"green",shape:"dot",text:(reqOpts.observe ? "Observed data ":"Data ")+"received ("+new Date().toLocaleTimeString()+")"});
-                    if ( node.options.rawBuffer ) {
-                        _send(data);
-                    } else if (res.headers['Content-Format'] === 'text/plain') {
-                        _send(data.toString());
-                    } else if (res.headers['Content-Format'] === 'application/json') {
-                        _send(JSON.parse(data.toString()));
-                    } else if (res.headers['Content-Format'] === 'application/cbor') {
-                        cbor.decodeAll(data, function (err, data) {
-                            if (err) {
-                                return false;
-                            }
-                            _send(data[0]);
-                        });
-                    } else if (res.headers['Content-Format'] === 'application/link-format') {
-                        _send(linkFormat.parse(data.toString()));
-                    } else {
-                        _send(data.toString());
-                    }
-                }
-
-                res.on('data', _onResponseData);
-
-                if (reqOpts.observe) {
-                    node.stream = res;
-                }
-            }
-
-            var payload = _constructPayload(msg, node.options.contentFormat);
-
-            if (node.options.observe === true) {
-                reqOpts.observe = '1';
-            } else {
-                delete reqOpts.observe;
-            }
-
-            //TODO: should revisit this block
-            if (node.stream) {
-                node.stream.close();
-            }
-
-            var req = coap.request(reqOpts);
-            req.on('response', _onResponse);
-            req.on('error', function(err) {
-                node.log('client error');
-                node.log(err);
-				node.status({fill:"red",shape:"ring",text:"Error:"+err});
-            });
-			req.on('timeout', function() {
-				node.status({fill:"red",shape:"ring",text:"Timeout"});
-            });
-			
-			node.status({fill:"yellow",shape:"ring",text:"Pending ("+(reqOpts.observe ? "Observed ":"")+reqOpts.method+" request)"});
-
-            if (payload) {
-                req.write(payload);
-            }
-            req.end();
-        }
-
-        this.on('input', function(msg) {
-            _makeRequest(msg);
-        });
-		
-		this.on('close', function(done) {
-			if (node.stream) {
-				node.stream.close();
-            }
-			done();
-        });
-		
-		node.status({});
-		
-		if(node.options.observe && node.options.observe_on_start){
-			var dummyPayload={payload:""};
-			_makeRequest(dummyPayload);
-		}
+    function constructPayload(msg, contentFormat) {
+      switch (contentFormat) {
+        case 'text/plain':
+          return msg.payload;
+        case 'application/json':
+          return JSON.stringify(msg.payload);
+        case 'application/cbor':
+          return cbor.encode(msg.payload);
+        default:
+          return null;
+      }
     }
-    RED.nodes.registerType("coap request", CoapRequestNode);
+
+    function makeRequest(msg) {
+      const reqOpts = url.parse(node.options.url || msg.url);
+      reqOpts.pathname = reqOpts.path;
+      reqOpts.method = (node.options.method || msg.method || 'GET').toUpperCase();
+      reqOpts.headers = {};
+      reqOpts.headers['Content-Format'] = node.options.contentFormat;
+
+      function onResponse(res) {
+        function send(payload) {
+          node.send(Object.assign({}, msg, {
+            payload,
+            headers: res.headers,
+            statusCode: res.code,
+          }));
+        }
+
+        function onResponseData(data) {
+          node.status({
+            fill: 'green',
+            shape: 'dot',
+            text: `${reqOpts.observe ? 'Observed data ' : 'Data '}received (${new Date().toLocaleTimeString()})`,
+          });
+          if (node.options.rawBuffer) {
+            send(data);
+          } else if (res.headers['Content-Format'] === 'text/plain') {
+            send(data.toString());
+          } else if (res.headers['Content-Format'] === 'application/json') {
+            send(JSON.parse(data.toString()));
+          } else if (res.headers['Content-Format'] === 'application/cbor') {
+            cbor.decodeAll(data).then(decodedData => {
+              send(decodedData[0]);
+            });
+          } else if (res.headers['Content-Format'] === 'application/link-format') {
+            send(linkFormat.parse(data.toString()));
+          } else {
+            send(data.toString());
+          }
+        }
+
+        res.on('data', onResponseData);
+
+        if (reqOpts.observe) {
+          node.stream = res;
+        }
+      }
+
+      const payload = constructPayload(msg, node.options.contentFormat);
+
+      if (node.options.observe === true) {
+        reqOpts.observe = '1';
+      } else {
+        delete reqOpts.observe;
+      }
+
+      // TODO: should revisit this block
+      if (node.stream) {
+        node.stream.close();
+      }
+
+      const req = coap.request(reqOpts);
+      req.on('response', onResponse);
+      req.on('error', err => {
+        node.log('client error');
+        node.log(err);
+        node.status({ fill: 'red', shape: 'ring', text: `Error:${err}` });
+      });
+      req.on('timeout', () => {
+        node.status({ fill: 'red', shape: 'ring', text: 'Timeout' });
+      });
+
+      node.status({
+        fill: 'yellow',
+        shape: 'ring',
+        text: `Pending (${reqOpts.observe ? 'Observed ' : ''}${reqOpts.method} request)`,
+      });
+
+      if (payload) {
+        req.write(payload);
+      }
+      req.end();
+    }
+
+    this.on('input', msg => {
+      makeRequest(msg);
+    });
+
+    this.on('close', done => {
+      if (node.stream) {
+        node.stream.close();
+      }
+      done();
+    });
+
+    node.status({});
+
+    if (node.options.observe && node.options.observe_on_start) {
+      const dummyPayload = { payload: '' };
+      makeRequest(dummyPayload);
+    }
+  }
+
+  RED.nodes.registerType('coap request', CoapRequestNode);
 };
